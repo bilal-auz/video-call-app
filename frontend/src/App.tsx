beec0e5914557c5ba25c143051fa058bd698214a
Bilal Abouzid <imbilalabouzid@gmail.com>
2023-12-24 13:02:51 +0200

    Fix guest connection and call acceptance state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
 import "./App.css";
-import { set } from "mongoose";
 const Peer = require("simple-peer");
 
 const socket = io("http://localhost:8080");
@@ -11,8 +10,8 @@ function App() {
   const [myId, setMyId] = useState("");
   const [userNameSubmitted, setUserNameSubmitted] = useState(false);
   const [state, setState] = useState("offline");
-  const [guestConnected, setGuestConnected] = useState(true);
-  const [guestName, setGuestName] = useState("John Doe");
+  const [guestConnected, setGuestConnected] = useState(false);
+  const [guestName, setGuestName] = useState("");
   const [guestId, setGuestId] = useState("");
   const [micOn, setMicOn] = useState(false);
   const [speakerOn, setSpeakerOn] = useState(false);
@@ -22,16 +21,19 @@ function App() {
     callerName: "",
     signalData: {},
   });
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
   const [stream, setStream] = useState<MediaStream>();
   const myVideoRef = useRef<HTMLVideoElement>(null);
   const guestRef = useRef<HTMLVideoElement>(null);
+  const peerRef = useRef<any>(null);
 
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
       .then((stream) => {
         setStream(stream);
-        if (myVideoRef.current && guestRef.current) {
+        if (myVideoRef.current) {
           myVideoRef.current.srcObject = stream;
         }
       });
@@ -47,6 +49,27 @@ function App() {
       setGuestConnected(true);
       setGuestName(callerName);
       setCall({ isReceivingCall: true, callerID, callerName, signalData });
+    });
+
+    socket.on("callEnd", () => {
+      console.log("callEnd");
+      setState("offline");
+      setGuestConnected(false);
+      setCallAccepted(false);
+      setCallEnded(true);
+      // setCall({
+      //   isReceivingCall: false,
+      //   callerID: "",
+      //   callerName: "",
+      //   signalData: {},
+      // });
+
+      // Destroy the peer connection
+      // peerRef.current.destroy();
+      // Set the peerRef.current to null after destruction
+      // peerRef.current = null;
+
+      window.location.reload();
     });
   }, []);
 
@@ -85,35 +108,49 @@ function App() {
     });
 
     //wait until the guest accept the call trigger the answer signal and start sharing
-    socket.on("callAccepted", (signal) => {
+    socket.on("callAccepted", ({ signal, callerName }) => {
       console.log("callAccepted: ", signal);
+      setCallAccepted(true);
+      setGuestName(callerName);
       peer.signal(signal);
     });
+
+    peerRef.current = peer;
   };
 
   const answerCall = () => {
     //when user accept the call
+    setState("calling");
+    setCallAccepted(true);
 
     //init the peer with data->stream
     const peer = new Peer({ initiator: false, trickle: false, stream });
 
     //triger the signal event and emit the answerCall event to the server to trigger the callAccepted event
     peer.on("signal", (data: any) => {
-      socket.emit("answerCall", { signal: data, to: call.callerID });
+      socket.emit("answerCall", {
+        signal: data,
+        to: call.callerID,
+        callerName: call.callerName,
+      });
     });
 
     //when the caller recive the answerCall event the data passed to peer init is passed here
     peer.on("stream", (stream: any) => {
-      if (guestRef.current) guestRef.current.srcObject = stream;
+      console.log("stream: ", stream);
+      if (peerRef.current && guestRef.current)
+        guestRef.current.srcObject = stream;
     });
 
     //pass the signal data to the peer and accept the peering
     peer.signal(call.signalData);
+
+    peerRef.current = peer;
   };
 
   const hangUp = () => {
-    setState("offline");
-    setGuestConnected(false);
+    socket.emit("forceDisconnect");
+    window.location.reload();
   };
 
   const handleMic = () => {
@@ -129,21 +166,21 @@ function App() {
       <main className="h-screen w-screen flex flex-col justify-center items-center ">
         <div className="flex flex-col w-[70%]">
           <div className="flex flex-row justify-center items-center">
-            <div className="flex justify-center items-center mr-5 w-80 h-60 relative rounded">
+            <div className="flex justify-center items-center mr-5 w-80 h-60 relative rounded-lg">
               <video
                 playsInline
                 autoPlay
                 ref={myVideoRef}
-                className="rounded w-80 [transform:rotateY(180deg)]"
+                className="rounded-lg w-80 [transform:rotateY(180deg)]"
               ></video>
               <p className="absolute bottom-0 left-0 bg-black px-1 text-xs rounded-bl">
-                {userName && userNameSubmitted && userName}
+                {userNameSubmitted && userName}
               </p>
               {!userNameSubmitted && (
                 <input
                   type="text"
                   placeholder="Your name..."
-                  className="input input-ghost w-full max-w-xs absolute bottom-0 focus:bg-transparent focus:border-0 focus:border-t-none focus:outline-none"
+                  className="input input-ghost w-full max-w-xs absolute bottom-0 focus:border-0 focus:border-t-none focus:outline-none rounded-t-none bg-[#00000070] text-gray-100 focus:text-gray-100 placeholder:text-gray-100"
                   onChange={(e) => {
                     setUserName(e.target.value);
                   }}
@@ -151,29 +188,34 @@ function App() {
                 />
               )}
             </div>
-            <div className="flex flex-row justify-center items-center relative w-80 h-60 bg-gray-900 rounded">
+
+            <div className="flex flex-row justify-center items-center relative w-80 h-60 bg-gray-900 rounded-lg">
               {guestConnected && (
                 <>
                   <video
                     playsInline
                     autoPlay
                     ref={guestRef}
-                    className="rounded [transform:rotateY(180deg)]"
+                    className="rounded-lg [transform:rotateY(180deg)]"
                   ></video>
                   <p className="absolute bottom-0 left-0 bg-black px-1 text-xs rounded-bl">
-                    {guestName && guestConnected && guestName}
+                    {guestName}
                   </p>
                 </>
               )}
 
-              {call.isReceivingCall && (
-                <div>
-                  {call.callerName} is Calling
-                  <button onClick={answerCall}>Answer</button>
+              {(!callAccepted && call.isReceivingCall && (
+                <div className="absolute">
+                  <p>{call.callerName} is Calling</p>
+                  <button onClick={answerCall} className="btn btn-xs">
+                    Answer
+                  </button>
                 </div>
-              )}
+              )) ||
+                (!guestConnected && <p className="absolute">Offline</p>)}
             </div>
           </div>
+
           <div className="flex flex-col justify-center items-center w-full mt-5">
             {(state == "calling" && (
               <div className="flex flex-row">
