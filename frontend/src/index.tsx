7ef20fdcc9a8585a8aeaa36add001ae46f7b7081
Bilal Abouzid <imbilalabouzid@gmail.com>
2024-01-17 23:25:28 +0100

    Add scrollbar styling & Remove strictmode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <VideoCallContext>
-      <App />
-    </VideoCallContext>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <VideoCallContext>
+    <App />
+  </VideoCallContext>
+  // </React.StrictMode>
 );
